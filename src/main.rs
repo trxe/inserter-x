1c8e97d7b93a53279ca79d35f4c05cc380c5296e
trxe <peixianwong@gmail.com>
2025-06-06 11:24:59 +0800

    init sandbox

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use argh::FromArgs;
+use polars::prelude::*;
+use polars_arrow::array::{Array, NullArray, PrimitiveArray, Utf8ViewArray};
+
+
+#[derive(FromArgs)]
+struct Args {
+    #[argh(option, short = 'f')]
+    pub filepath: String,
+    #[argh(option, short = 'h')]
+    pub url: String,
+}
+
+struct CArrowColumn {
+    //  pub array: Vec<polars_arrow::ffi::ArrowArray>,
+    //  pub schema: polars_arrow::ffi::ArrowSchema,
+    pub array: Vec<Box<dyn Array>>,
+    pub schema: ArrowField,
+}
+
+impl CArrowColumn {
+    pub fn new(c: polars::prelude::Column) -> Self {
+        let series = c.take_materialized_series();
+        let field = series.field().to_arrow(CompatLevel::newest());
+        let arrow_chunks = series.into_chunks();
+        //  let c_field = export_field_to_c(&field);
+        //  let c_array = arrow_chunks.into_iter().map(export_array_to_c).collect();
+        Self { array: arrow_chunks, schema: field }
+    }
+}
+
+fn convert(frame: DataFrame, args: Args) {
+    // let columns = frame.take_columns();
+    // let chclient = clickhouse_rs::Pool::new(args.url);
+    frame.iter_chunks(CompatLevel::newest(), false).for_each(|chunk| {
+
+    });
+    // let carr = columns.into_iter().map(CArrowColumn::new).collect::<Vec<_>>();
+}
+
+fn main() {
+    let args: Args = argh::from_env();
+    let reader = LazyCsvReader::new(std::path::Path::new(&args.filepath));
+    let lf = reader.finish().expect("invalid frame filepath");
+    let df = lf.collect().unwrap();
+    println!("{}\n{:?}", &args.filepath, df);
+    convert(df, args);
+}
